cb8fb7838c69b08e1ec981945a5a9c38082f7c3d
Tulsidai <tulsidai.singhh@gmail.com>
2026-04-30 10:38:22 -0400

    add report, update renv lockfile, update pipeline to include report

--- run_pipeline.R
+++ run_pipeline.R
@@ -8,22 +8,25 @@ source("R/00_config.R")
 dir.create(p_out, recursive = TRUE, showWarnings = FALSE)
 dir.create(p_log, recursive = TRUE, showWarnings = FALSE)
 
-cat("1/6 ~ Ingesting raw data\n")
+cat("1/7 ~ Ingesting raw data\n")
 source("R/01_ingest.R")
 
-cat("2/6 ~ Running diagnostics\n")
+cat("2/7 ~ Running diagnostics\n")
 source("R/02_diagnose.R")
 
-cat("3/6 ~ Cleaning...\n")
+cat("3/7 ~ Cleaning...\n")
 source("R/03_clean.R")
 
-cat("4/6 ~ Imputing\n")
+cat("4/7 ~ Imputing\n")
 source("R/04_impute.R")
 
-cat("5/6 ~ Exporting\n")
+cat("5/7 ~ Exporting\n")
 source("R/05_export.R")
 
-cat("6/6 ~ Building data dictionary\n")
+cat("6/7 ~ Building data dictionary\n")
 source("R/data_dictionary.R")
+
+cat("7/7 ~ Rendering report\n")
+quarto::quarto_render("R/06_report.qmd", output_file = "../outputs/report.docx")
 
 cat("\nPipeline complete. All outputs written to outputs/\n")
